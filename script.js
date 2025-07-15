948c83568ecbc1889f828bc3d550af432bee3d78
Meggie Nguyen <meggien1@uci.edu>
2025-07-15 14:52:34 -0700

    add lightbox

---
+++ script.js
@@ -0,0 +1,38 @@
+const filterButtons = document.querySelectorAll('.filters button');
+const images = document.querySelectorAll('.gallery img');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    const filter = button.getAttribute('data-filter');
+
+    images.forEach(img => {
+        const category = img.getAttribute('data-category');
+        if (filter === 'all' || category === filter) {
+        img.classList.remove('hidden');
+        } else {
+        img.classList.add('hidden');
+        }
+    });
+    });
+});
+
+// LIGHTBOX FUNCTION
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+    lightboxImg.alt = img.alt;
+  });
+});
+
+// CLOSE LIGHTBOX
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+// ALSO close lightbox if you click outside the image
+lightbox.addEventListener('click', (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
